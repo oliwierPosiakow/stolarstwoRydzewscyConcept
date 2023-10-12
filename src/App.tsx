54047a1c461da7606a09182e9a4a4f5d77b8bb37
maleckyy <kupferpatryk@gmail.com>
2023-10-12 17:17:59 +0200

    footer, no styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./app.css";
+import Footer from "./components/footer/Footer.tsx";
 import Navbar from "./components/navbar/Navbar.tsx";
 
 function App(): JSX.Element {
   return (
     <>
-        <Navbar/>
+      <Navbar />
       <h1>App</h1>
+      <Footer />
     </>
   );
 }

c9f2ef862eefcc19f38cb4b5af9d21a45f4dadde
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-10-16 20:04:23 +0200

    Minor changes

--- src/data/comments.ts
+++ src/data/comments.ts
@@ -4,10 +4,10 @@ export interface CommentData {
 
 export const comments: CommentData[] = [
     {
-        content: 'Zleciłem wykonanie dodatkowej pary drzwi wejściowych, a także przeniesienie zabytkowych drzwi na nową ościeżnicę. Jesteśmy zadowoleni z  jakości  wykonania, szybkiej realizacji i dobrej współpracy.'
+        content: 'Zleciłem wykonanie dodatkowej pary drzwi wejściowych, a także przeniesienie zabytkowych drzwi na nową ościeżnicę. Jesteśmy zadowoleni z jakości  wykonania, szybkiej realizacji i dobrej współpracy.'
     },
     {
-        content: 'Super expresowa obsługa. Przyniosłam swój materiał do przycięcia i wycięcia odpowiednich kształtów,  ciężko znaleźć stolarza , który by to wykonał z materiału nie zakupionego na miejscu. Fachowe wykonanie, polecam.'
+        content: 'Super expresowa obsługa. Przyniosłam swój materiał do przycięcia i wycięcia odpowiednich kształtów, ciężko znaleźć stolarza, który by to wykonał z materiału niezakupionego na miejscu. Fachowe wykonanie, polecam.'
     },
     {
         content: 'Wspaniała jakość wykonania oraz materiałów. Jestem bardzo zadowolona .Barierka wygląda rewelacyjnie. Szczerze polecam!'

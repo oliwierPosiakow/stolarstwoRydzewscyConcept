2eaefd4dbe5034f98908ef4bad14345cc3bf705d
maleckyy <kupferpatryk@gmail.com>
2023-10-16 18:17:28 +0200

    navbar links, hero css

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,7 +3,7 @@ import "../../assets/styles/colors.scss";
 
 export default function Contact() {
   return (
-    <div className="contact_wrapper">
+    <div id="contact" className="contact_wrapper">
       <div className="contact_wrapper--map-panel">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1161.9517941439285!2d18.622220952321655!3d54.3763532370265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fd7498555308d9%3A0x55f01bc1325ad31d!2sStolarstwo%20Ojc.-Syn%20Rydzewscy!5e0!3m2!1spl!2spl!4v1697045738786!5m2!1spl!2spl"
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,21 +1,20 @@
-import './hero.css';
+import "./hero.css";
 
 export default function Hero() {
-    return (
-        <div className="hero">
-            <div className="content-wrapper">
-                <div className="hero-content">
-                    <h2 className={"hero-content_title"}>
-                        Najwyższa jakość wyrobów stolarskich
-                    </h2>
-                    <p className="hero-content_text">
-                        Odkryj świat wyrobów stolarskich wykonywanych z pasją, dostosowanych do twoich potrzeb
-                    </p>
-                    <div className="hero-content_button">
-                        Zobacz więcej
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="home" className="hero">
+      <div className="content-wrapper">
+        <div className="hero-content">
+          <h2 className={"hero-content_title"}>
+            Najwyższa jakość wyrobów stolarskich
+          </h2>
+          <p className="hero-content_text">
+            Odkryj świat wyrobów stolarskich wykonywanych z pasją, dostosowanych
+            do twoich potrzeb
+          </p>
+          <div className="hero-content_button">Zobacz więcej</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }

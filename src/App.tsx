53ee827575aed89f959a831ede255aeceabf6da4
maleckyy <kupferpatryk@gmail.com>
2023-10-17 17:50:28 +0200

    gallery component/ responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import Contact from "./components/contact/Contact.tsx";
 import Footer from "./components/footer/Footer.tsx";
 import Navbar from "./components/navbar/Navbar.tsx";
 import Comments from "./components/comments/Comments.tsx";
-import Hero from "./components/hero/Hero.tsx"
+import Hero from "./components/hero/Hero.tsx";
+import Products from "./components/products/Products.tsx";
 
 function App(): JSX.Element {
   return (
     <>
-        <Navbar />
-        <Hero />
-        <Comments/>
-        <Contact />
-        <Footer />
+      <Navbar />
+      <Hero />
+      <Products />
+      <Comments />
+      <Contact />
+      <Footer />
     </>
   );
 }

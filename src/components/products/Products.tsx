8b1ee664b69bdb54672928e5df290a91a6665ee0
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-10-20 14:32:51 +0200

    Some fixes to Products

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,17 +1,22 @@
 import "./products.css";
+import ScrollContainer from "react-indiana-drag-scroll";
+import 'react-indiana-drag-scroll/dist/style.css';
 
 function ProductDetails(props: any) {
   return (
-    <img
-      className="product-image"
-      src={`./productsData/${props.imgSrc}.jpg`}
-      alt="props.imgSrc"
-    />
+      <div>
+          <img
+              className="product-image"
+              src={`./productsData/${props.imgSrc}.jpg`}
+              alt="props.imgSrc"
+          />
+      </div>
   );
 }
 
-export default function Products() {
-  const imagesArr: Array<string> = [
+export default function Products(): JSX.Element {
+
+    const imagesArr: Array<string> = [
     "door1",
     "door2",
     "altana1",
@@ -19,15 +24,16 @@ export default function Products() {
     "stairs2",
     "stairs3",
     "stairs4",
-  ];
-  return (
+    ];
+
+    return (
     <div className="products-container" id="gallery">
-      <div className="products-container--title">Nasze produkty</div>
-      <div className="images-wrapper">
+      <div className="products-container--title">Nasze realizacje</div>
+      <ScrollContainer className="images-wrapper" hideScrollbars={false} mouseScroll={true}>
         {imagesArr.map((image: string) => {
           return <ProductDetails imgSrc={image} key={image} />;
         })}
-      </div>
+      </ScrollContainer>
       <a
         href="https://www.facebook.com/profile.php?id=100031545596353&sk=photos"
         target="_blank"
